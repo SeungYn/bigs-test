fdd8acf07a5e1d9df0ee46ad8e117b720e054bca
SeungYn <kwls0407@gmail.com>
2024-05-07 18:44:39 +0900

    feat: restTemplate 빈 추가

--- bigs-util/src/main/java/org/bigs/fetch/RestTemplateConfig.java
+++ bigs-util/src/main/java/org/bigs/fetch/RestTemplateConfig.java
@@ -4,12 +4,19 @@ import lombok.RequiredArgsConstructor;
 import org.bigs.interceptor.LoggingFetchInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
 
 @RequiredArgsConstructor
 @Configuration
-public class RestTemplate {
+public class RestTemplateConfig {
     private final LoggingFetchInterceptor loggingFetchInterceptor;
 
     @Bean
-    public 
+    public RestTemplate restTemplate(){
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setInterceptors(Collections.singletonList(loggingFetchInterceptor));
+        return restTemplate;
+    }
 }
