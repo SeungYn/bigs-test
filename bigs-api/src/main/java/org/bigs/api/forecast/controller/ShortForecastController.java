4c540aeeaaf05ef69d396ccd56d7120d4e9630e6
SeungYn <kwls0407@gmail.com>
2024-05-09 02:14:50 +0900

    feat: ShortForecastController 지역명을 입력 받아 적재 및 가져오는 컨트롤러 추가

--- bigs-api/src/main/java/org/bigs/api/forecast/controller/ShortForecastController.java
+++ bigs-api/src/main/java/org/bigs/api/forecast/controller/ShortForecastController.java
@@ -3,9 +3,7 @@ package org.bigs.api.forecast.controller;
 import lombok.RequiredArgsConstructor;
 import org.bigs.api.forecast.dto.ShortForecastDTO;
 import org.bigs.api.forecast.service.ShortForecastService;
-import org.bigs.domain.forecast.entity.ShortForecast;
 import org.bigs.util.common.CommonResponse;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -43,12 +41,32 @@ public class ShortForecastController {
                 .build());
     }
 
-    // kakaoapi를 사용해 해당 지역의 위도, 경도를 구한 후
-    // location db에서 nx, ny를 구한 후 해당 좌표의 날씨를 가져오는 메서드 구현 중...
+    // 지역명을 입력 받아 해당 지역의 날씨를 구해옴
     @GetMapping("/short-local/{location}")
-    public ResponseEntity<?> getLocalSHortForecast(@PathVariable("location") String location){
-        shortForecastService.getLocalShortForecast(location);
-        return ResponseEntity.status(HttpStatus.OK).build();
+    public ResponseEntity<List<ShortForecastDTO.ShortForecastRes>> getLocalSHortForecast(@PathVariable("location") String location){
+        List<ShortForecastDTO.ShortForecastRes> shortForecasts = shortForecastService.getLocalShortForecast(location);
+
+        if(shortForecasts.size() == 0) return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        return ResponseEntity.status(HttpStatus.OK).body(shortForecasts);
+    }
+
+    // 지역명을 입력 받아 해당 지역의 날씨를 저장
+    @PostMapping("/short-local/{location}")
+    public ResponseEntity<CommonResponse> postLocalShortForecast(@PathVariable("location") String location){
+        Boolean res = shortForecastService.loadLocalForecast(location);
+        if(!res) return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(CommonResponse.builder()
+                .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message("잠시 후 다시 시도해주세요")
+                .success(false)
+                .data(null)
+                .build());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(CommonResponse.builder()
+                .code(HttpStatus.CREATED.value())
+                .message("저장 완료!")
+                .success(true)
+                .data(null)
+                .build());
     }
 
 }
