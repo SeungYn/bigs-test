b73184655c9d99f9270fd79a892413a63d9140ab
SeungYn <kwls0407@gmail.com>
2024-05-08 11:03:57 +0900

    api모듈 application.yml 및 시작 클래스 작성

---
+++ bigs-api/src/main/java/org/bigs/api/BigsAPIApplication.java
@@ -0,0 +1,14 @@
+package org.bigs.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = {"org.bigs"})
+@SpringBootApplication()
+public class BigsAPIApplication {
+    public static void main(String[] args){
+        SpringApplication.run(BigsAPIApplication.class);
+    }
+}
